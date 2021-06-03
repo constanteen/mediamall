2e4a9617326099e33981d27314724fece98a99ea
Chilo Nwokolo <chilo.nwokolo@kodehauz.com>
2021-06-03 08:08:31 +0000

    Merged in ft-move-app-to-nextjs (pull request #2)
    ft move to nextjs
    
    * ft move to nextjs
    
    * change FMM to face of eket
    
    * remove .idea from gitignore
    
    * remove idea
    
    Approved-by: Richie Nabuk

---
+++ pages/face-of-eket.tsx
@@ -0,0 +1,67 @@
+export default function index() {
+  return (
+    <main>
+      <div className="mb-4">
+        <img src="/images/fmm.png" alt="" className="w-full" />
+      </div>
+      <div className="md:px-16 lg:px-16 px-4 pb-10">
+        <div className="lg:grid md:grid-cols-4 grid-cols-2 gap-10 border-b-2 pb-16 pt-20">
+          <div className="col-span-2">
+            <img src="/images/queen.png" alt="" className="" />
+          </div>
+          <div className="col-span-2">
+            <div className="mb-2">
+              <div className="border-b-2 mb-5">
+                <h4 className="text-2xl font-bold py-4">Sediong Ukoka</h4>
+                <p className="pb-3 space-x-7">
+                  <span>Hair Color :<b> Black</b></span>
+                  <span>Eye Color :<b> Black</b></span>
+                </p>
+              </div>
+              <div className="border-b-2 pb-5">
+                <h4 className="font-bold mb-3 text-4xl">About</h4>
+                <p>The best thing was learning from people that cared enough to go beyond their means to guarantee you
+                  learThe best thing was learning from people that cared enough to go beyond their means to.
+                  The best thing was learning from people that cared enough to go beyond their means to guarantee you
+                  learn.
+                  The best thing was learning from people that cared enough to go beyond their means to guarantee you
+                  learThe best thing was learning from people that cared enough to go beyond their means to.</p>
+              </div>
+              <div className="grid md:grid-cols-3 gap-9 mt-6">
+                <div className="col-span-1">
+                  <img src="/queen.png" alt="" className="hover:opacity-75" />
+                </div>
+                <div className="col-span-1">
+                  <img src="/queen.png" alt="" className="hover:opacity-75" />
+                </div>
+                <div className="col-span-1">
+                  <img src="/queen.png" alt="" className="hover:opacity-75" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:px-16 lg:px-16 px-4">
+            <div className="grid md:grid-cols-4 gap-9 mt-6">
+              <div className="col-span-1">
+                <img src="/images/model.png" alt="" className="hover:opacity-75" />
+                  <p className="text-center font-bold my-2">Sediong Ekong</p>
+              </div>
+              <div className="col-span-1">
+                <img src="/images/model.png" alt="" className="hover:opacity-75" />
+                  <p className="text-center font-bold my-2">Princess</p>
+              </div>
+              <div className="col-span-1">
+                <img src="/images/model.png" alt="" className="hover:opacity-75" />
+                  <p className="text-center font-bold my-2">Queen Jack</p>
+              </div>
+              <div className="col-span-1">
+                <img src="/images/model.png" alt="" className="hover:opacity-75" />
+                  <p className="text-center font-bold my-2">Ann Emma</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
